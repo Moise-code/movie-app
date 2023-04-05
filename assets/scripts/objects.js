9e1867cfa1e0531feec3b82c3e084646d91b098a
Moise-code <moisewowe@gmail.com>
2023-04-05 11:21:59 +0200

    adding the function to render the movies to the User interface

--- assets/scripts/objects.js
+++ assets/scripts/objects.js
@@ -4,12 +4,34 @@ const addMovieBtn = document.querySelector('#add-movie-btn');
 const searchBtn = document.querySelector('#search-btn');
 const movies = [];
 
+// now lets create the the function to render the movie list to the UI
+const renderMovies = () =>{
+  const movieList = document.querySelector('#movie-list');
+  if(movies.length === 0){
+    movieList.classList.remove('visible');
+    return;
+  } else{
+    movieList.classList.add('visible');
+  }
+  movieList.innerHTML = '';
+  movies.forEach((movie) =>{
+    const movieEl = document.createElement('li');
+    let movieContent = movie.info.title + ' - ';
+    for(const key in movie.info){
+      if(key !== 'title'){
+        movieContent += `${key} ${movie.info[key]}`;
+      }
+    }
+    movieEl.textContent = movieContent;
+    movieList.prepend(movieEl); 
+  });
+}
+
 // lets create the function to grab user in puts
 const addMovieHandler = () =>{
   const title = document.querySelector('#title').value;
   const extraName = document.querySelector('#extra-name').value;
   const extraValue = document.querySelector('#extra-value').value;
-
   if(title.trim() === '' || extraName.trim() === '' || extraValue.trim() === '') {
     return;
   }
@@ -22,6 +44,7 @@ const addMovieHandler = () =>{
   id:Math.random()
   }
   movies.push(newMovie);
+  renderMovies();
   console.log(newMovie);
 }
 
